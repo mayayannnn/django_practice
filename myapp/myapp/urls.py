7ee6db98a90f266c32195c64b53afa5607587c93
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-09 21:01:49 +0900

    create todo

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -20,5 +20,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("test/", include("app.urls"))
+    path("test/", include("app.urls")),
+    path("todo/", include("todo.urls"))
 ]
