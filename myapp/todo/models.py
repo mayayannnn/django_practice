963958feff0d6a6826a747d41551ad3158080c5d
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-09 21:18:20 +0900

    add datatable

--- myapp/todo/models.py
+++ myapp/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    memo = models.CharField("memo",max_length=225)
+
+    def __str__(self):
+        return self.memo
--- myapp/todo/views.py
+++ myapp/todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+# from todo.models import Todo
 
 # Create your views here.
 
 def index(request):
+    # todo = Todo(memo = "memo")
+    # todo.save()
     return render(request, "index.html")
